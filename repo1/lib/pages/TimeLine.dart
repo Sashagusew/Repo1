13d2df1dfa575cb6698ae8a89b2effee08ed97e6
Tatyana-Essentya <dlyarubicona@gmail.com>
2021-10-27 19:23:47 +0300

    Комит

--- repo1/lib/pages/TimeLine.dart
+++ repo1/lib/pages/TimeLine.dart
@@ -12,14 +12,73 @@ class _TimeLinePegeState extends State<TimeLinePege> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size; //считывает размер устройства
     return SafeArea(
+      //не залазиет на кнопки системы и верхнюю бровь
       child: Scaffold(
         body: PageView(
+          //создание тик-тока
           controller: controller,
           physics: BouncingScrollPhysics(),
           scrollDirection: Axis.vertical,
-          children: [],
+          children: [
+            Container(
+              color: Colors.redAccent,
+              height: size.height,
+              width: size.width,
+              child: Center(
+                child: Text(
+                  "Привет!",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              height: size.height,
+              width: size.width,
+              child: Center(
+                child: Text(
+                  "Бегите глупцы",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              height: size.height,
+              width: size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Ratata",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    "Ratata",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
